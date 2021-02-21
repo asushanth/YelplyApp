6c864b7ec660001909c43bd915cb9b2d9c5a70f5
Adarsh Sushanth <adarsh_sushanth@yahoo.com>
2021-02-21 14:04:07 -0500

    Yelply App

---
+++ Yelpy/Controllers/Main.swift
@@ -0,0 +1,30 @@
+//
+//  Main.swift
+//  Yelpy
+//
+//  Created by Adarsh Sushanth on 2/21/21.
+//  Copyright © 2021 memo. All rights reserved.
+//
+
+import UIKit
+
+class Main: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
